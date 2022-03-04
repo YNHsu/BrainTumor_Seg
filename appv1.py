12728296f867acce2a5ec06e99bab8589328687a
YNHsu <84616635+YNHsu@users.noreply.github.com>
2022-03-04 11:14:20 +0800

    Update appv1.py

--- appv1.py
+++ appv1.py
@@ -6,16 +6,17 @@ import time
 
 
 # App Title
-st.title("Brain tumor segmentation APP")
+st.title("Brain tumor segmentation Web")
 
-st.markdown(unsafe_allow_html=True, body="<p>This is a basic app built with Streamlit.")
+st.markdown(unsafe_allow_html=True, body="<p>With this web, you can select a CT image of the brain and predict the tumor regions.</p>"
+                                         "<p>The model used is a High-resolution Network(HRNet) that outperforms UNet and UNet++ models.</p>")
                                         
-
 
 # =============================================================================
 # img = st.file_uploader(label='Load a CT brain image', type=['png','jpg','jpeg'], key='CT', accept_multiple_files=False)
 # basename = img.name
 # =============================================================================
+
 
 imgname = st.selectbox('Select the CT image', (None, 'CTbrain_1.png','CTbrain_2.png','CTbrain_3.png','CTbrain_4.png','CTbrain_5.png','CTbrain_6.png','CTbrain_7.png','CTbrain_8.png'))
 
@@ -23,9 +24,10 @@ imgname = st.selectbox('Select the CT image', (None, 'CTbrain_1.png','CTbrain_2.
 if imgname is not None:
     img = Image.open(f'CTimage/{imgname}')
     st.subheader('CT image')
-    st.image(img, width=340)
+    st.image(img, width=340, caption=f'{imgname}')
     
-    pred = st.button('Predict')
+    
+    pred = st.button('Click here to predict')
     if pred is True:
         st.text('Predicting...')
         col1, col2 = st.columns(2)
@@ -33,14 +35,10 @@ if imgname is not None:
         time.sleep(1)
         Segment = Image.open(f'CToverlay/{imgname}')
         col1.subheader('Segmentation')
-        col1.image(Segment, use_column_width=True, caption='Ground Truth(Red line)\nPrediction(Blue line)')
+        col1.image(Segment, use_column_width=True)
+        col1.text('Red line (Ground Truth)\nBlue line (Prediction)')
         
         time.sleep(1)
         GradCAM = Image.open(f'GradCAM/{imgname}')
         col2.subheader('GradCAM')
         col2.image(GradCAM, use_column_width=True)
-        
-
-
-
-    
